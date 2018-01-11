94d1924a2b4b38ba4291992939b401358a3e37ef
Matt Withum <mdw233@gmail.com>
2018-01-11 08:42:19 -0500

    I want to liiiiife-cycle

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,52 @@
 import React, { Component } from 'react';
 
-class Header extends Component {
+class Header extends React.Component {
     constructor(props) {
         super(props);
 
-        setInterval(() => 
-            this.props.handleChange({target: {value: "xxxxxx"}}) , 
-        3000);
+        setInterval(() => {
+            this.props.handleChange({target: {value: "xxxxxx"}});
+            this.props.addThing("--------");
+        }, 
+    3000);
     }    
 
+    componentWillMount() {
+        console.log("header: componentWillMount");
+    }   
+
+    componentDidMount() {
+        console.log("header: componentDidMount");
+    }  
+
+    componentWillReceiveProps(nextProps) {
+        console.log("header: componentWillReceiveProps");
+    }  
+
+    componentWillUpdate() {
+        console.log("header: componentWillUpdate");
+    } 
+
+    componentDidUpdate() {
+        console.log("header: componentDidUpdate");
+    }
+
+    shouldComponentUpdate() {
+        console.log("header: shouldComponentUpdate");
+        return true;
+    }
+
     render() {
+
+        let listItems = this.props.things.map((t) => <li>{t}</li>);
+
         return (
             <div>
                 <h1>header</h1>
                 <p>Junk is: {this.props.junk}</p>
+                <ul>
+                    {listItems}
+                </ul>
             </div>
         );
     }
--- src/scenes/Home/index.jsx
+++ src/scenes/Home/index.jsx
@@ -7,7 +7,8 @@ class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            junk: ""
+            junk: "",
+            things: ["a", "b"]
         };
     }
 
@@ -15,12 +16,25 @@ class Home extends React.Component {
         console.log(e.target.value);
         console.log(this);
         this.setState({junk: e.target.value});
+        this.addThing(e.target.value);
+    }
+
+    addThing(thing) {
+        let t = this.state.things;
+        let things = t.slice(0, t.length);
+        things.push(thing);
+        this.setState({things: things});
     }
 
     render() {
         return (
             <div>
-                <Header junk={this.state.junk} handleChange={(e) => this.handleChange(e)} />
+                <Header 
+                    junk={this.state.junk} 
+                    things={this.state.things} 
+                    addThing={(t) => this.addThing(t)} 
+                    handleChange={(e) => this.handleChange(e)} />
+
                 <div>This is home</div>
                 <input type="text" onChange={(e) => this.handleChange(e)} />
                 <div>{this.state.junk}</div>
