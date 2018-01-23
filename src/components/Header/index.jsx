f2d92577fa6dc17f8409e104147217ddcf78b614
Matt Withum <mdw233@gmail.com>
2018-01-23 07:46:01 -0500

    cleaned things up so that the app can be built; left some of the learning files in there for reference early on - they should be deleted later

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,50 +3,14 @@ import React, { Component } from 'react';
 class Header extends React.Component {
     constructor(props) {
         super(props);
-
-        setInterval(() => {
-        }, 
-    3000);
     }    
 
-    componentWillMount() {
-        console.log("header: componentWillMount");
-    }   
-
-    componentDidMount() {
-        console.log("header: componentDidMount");
-    }  
-
-    componentWillReceiveProps(nextProps) {
-        console.log("header: componentWillReceiveProps");
-    }  
-
-    componentWillUpdate() {
-        console.log("header: componentWillUpdate");
-    } 
-
-    componentDidUpdate() {
-        console.log("header: componentDidUpdate");
-    }
-
-    shouldComponentUpdate() {
-        console.log("header: shouldComponentUpdate");
-        return true;
-    }
-
     
-
     render() {
-
-        let listItems = this.props.things.map((t) => <li>{t}</li>);
 
         return (
             <div>
                 <h1>header</h1>
-                <p>Junk is: {this.props.junk}</p>
-                <ul>
-                    {listItems}
-                </ul>
             </div>
         );
     }
---
+++ src/scenes/Home/index-old.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import {createStore} from 'redux';
+
+import Header from '../../components/Header';
+import tnhApp from '../../services/fun/fun.reducer';
+import { haveFun } from '../../services/fun/fun.actions';
+
+let store = createStore(tnhApp);
+console.log('created store', store);
+
+class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            junk: "",
+            things: ["a", "b"],
+            funHad: 0
+        };
+
+        store.subscribe(() => {
+            this.setState({funHad: store.getState().funHad})
+        });
+    }
+
+    handleChange(e) {
+        console.log(e.target.value);
+        console.log(this);
+        this.setState({junk: e.target.value});
+        this.addThing(e.target.value);
+        store.dispatch(haveFun(3));
+    }
+
+    addThing(thing) {
+        let t = this.state.things;
+        let things = t.slice(0, t.length);
+        things.push(thing);
+        this.setState({things: things});
+    }
+
+    render() {
+        return (
+            <div>
+                <Header 
+                    junk={this.state.junk} 
+                    things={this.state.things} 
+                    addThing={(t) => this.addThing(t)} 
+                    handleChange={(e) => this.handleChange(e)} />
+
+                <div>This is home</div>
+                <input type="text" onChange={(e) => this.handleChange(e)} />
+                <div>{this.state.junk}</div>
+                <div>{this.state.funHad}</div>
+            </div>
+        );
+    }
+}
+
+export default Home;
