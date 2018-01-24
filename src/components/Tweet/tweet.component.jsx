169bcbf5e06ac0ed7c9b4806b47a4ad708450482
Matt Withum <mdw233@gmail.com>
2018-01-24 08:01:58 -0500

    added a few empty components to get a feel for layout

---
+++ src/components/Tweet/tweet.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const TweetComponent = ({}) => {
+    return (
+    <div>
+        I am a tweet ...
+    </div>
+    )
+}
+
+export default TweetComponent
--- src/scenes/Home/index.jsx
+++ src/scenes/Home/index.jsx
@@ -4,6 +4,7 @@ import {createStore} from 'redux';
 import Header from '../../components/Header';
 import tnhApp from '../../services/fun/fun.reducer';
 import { haveFun } from '../../services/fun/fun.actions';
+import TweetComponent from '../../components/Tweet/tweet.component';
 
 let store = createStore(tnhApp);
 console.log('created store', store);
@@ -19,6 +20,7 @@ class Home extends React.Component {
             <div>
                 <Header />
                 <p>Home page let's do things</p>
+                <TweetComponent />
             </div>
         );
     }
